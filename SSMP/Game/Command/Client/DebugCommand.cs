b0ecf6a350eea2197fc97a0a4658ab04fc290efb
Extremelyd1 <10898310+Extremelyd1@users.noreply.github.com>
2025-10-24 13:39:38 +0200

    Add debug command updates

--- SSMP/Game/Command/Client/DebugCommand.cs
+++ SSMP/Game/Command/Client/DebugCommand.cs
@@ -15,13 +15,44 @@ internal class DebugCommand : IClientCommand {
 
     /// <inheritdoc />
     public void Execute(string[] arguments) {
-        var sprintFsm = HeroController.instance.sprintFSM;
+        OutputBindFsmStateNames();
+    }
 
-        for (var i = 0; i < sprintFsm.FsmStates.Length; i++) {
-            var stateName = sprintFsm.FsmStates[i].name;
-            
-            Logger.Info($"{i}: {stateName}");
+    private void OutputBindFsmStateNames() {
+        var heroFsms = HeroController.instance.GetComponents<PlayMakerFSM>();
+        foreach (var heroFsm in heroFsms) {
+            if (heroFsm.FsmName == "Bind") {
+                for (var i = 0; i < heroFsm.FsmStates.Length; i++) {
+                    var state = heroFsm.FsmStates[i];
+                    
+                    Logger.Info($"{i}: {state.name}");
+                }
+
+                break;
+            }
         }
+    }
+
+    private void ListHeroControllerFsmNames() {
+        var hc = HeroController.instance;
+        Logger.Info($"sprintFSM: {hc.sprintFSM.name}, {hc.sprintFSM.fsm.name}");
+        Logger.Info($"sprintFSM: {hc.toolsFSM.name}, {hc.toolsFSM.fsm.name}");
+        Logger.Info($"sprintFSM: {hc.mantleFSM.name}, {hc.mantleFSM.fsm.name}");
+        Logger.Info($"sprintFSM: {hc.umbrellaFSM.name}, {hc.umbrellaFSM.fsm.name}");
+        Logger.Info($"sprintFSM: {hc.silkSpecialFSM.name}, {hc.silkSpecialFSM.fsm.name}");
+        Logger.Info($"sprintFSM: {hc.crestAttacksFSM.name}, {hc.crestAttacksFSM.fsm.name}");
+        Logger.Info($"sprintFSM: {hc.harpoonDashFSM.name}, {hc.harpoonDashFSM.fsm.name}");
+        Logger.Info($"sprintFSM: {hc.superJumpFSM.name}, {hc.superJumpFSM.fsm.name}");
+        Logger.Info($"sprintFSM: {hc.bellBindFSM.name}, {hc.bellBindFSM.fsm.name}");
+        Logger.Info($"sprintFSM: {hc.wallScrambleFSM.name}, {hc.wallScrambleFSM.fsm.name}");
+    }
+
+    private void EquipCloaklessCrest() {
+        ToolItemManager.AutoEquip(ToolItemManager.GetCrestByName("Cloakless"), true, true);
+    }
+
+    private void EquipCursedCrest() {
+        ToolItemManager.AutoEquip(ToolItemManager.GetCrestByName("Cursed"), true, true);
     }
 
     private void Test() {
