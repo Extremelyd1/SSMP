1643a8ce67f9389b226d07b7d1e0cbaf09812e27
Liparakis <stiliano446@gmail.com>
2025-11-30 09:55:35 +0200

    New struct, representing a connection.

---
+++ SSMP/Networking/ConnectionDetails.cs
@@ -0,0 +1,35 @@
+using SSMP.Networking.Transport.Common;
+
+namespace SSMP.Networking;
+
+/// <summary>
+/// Struct containing details required to establish a connection.
+/// </summary>
+public struct ConnectionDetails {
+    /// <summary>
+    /// The IP address or Steam ID to connect to.
+    /// </summary>
+    public string Address;
+
+    /// <summary>
+    /// The port to connect to (for UDP).
+    /// </summary>
+    public int Port;
+
+    /// <summary>
+    /// The username of the player connecting.
+    /// </summary>
+    public string Username;
+
+    /// <summary>
+    /// The transport type to use for the connection.
+    /// </summary>
+    public TransportType TransportType;
+
+    public ConnectionDetails(string address, int port, string username, TransportType transportType) {
+        Address = address;
+        Port = port;
+        Username = username;
+        TransportType = transportType;
+    }
+}
