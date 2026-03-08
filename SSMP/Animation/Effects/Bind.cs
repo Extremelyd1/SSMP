5b5d95da11ba17e8f3a48515a1cfe7565a5f8286
BobbyTheCatfish <46359040+BobbyTheCatfish@users.noreply.github.com>
2026-03-08 00:22:06 -0500

    Finalize basic bind start and end animations

--- SSMP/Animation/Effects/Bind.cs
+++ SSMP/Animation/Effects/Bind.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.Linq;
 using HutongGames.PlayMaker.Actions;
 using SSMP.Internals;
 using SSMP.Util;
@@ -12,22 +13,45 @@ namespace SSMP.Animation.Effects;
 /// <summary>
 /// Class for the animation effect of bind (healing).
 /// </summary>
-internal class Bind : AnimationEffect {
+internal class Bind : DamageAnimationEffect {
+
+    protected string BIND_BELL_NAME = "bind_bell_appear_instance";
+
+    protected class Flags {
+        public bool BindBell = false;
+        public bool BaseMirror = false;
+        public bool UpgradedMirror = false;
+        public bool QuickBind = false;
+        public bool ReserveBind = false;
+        public bool Maggoted = false;
+
+        public Flags(byte[]? info) {
+            if (info == null) return;
+            BindBell = info[0] == 1;
+            BaseMirror = info[1] == 1;
+            UpgradedMirror = info[2] == 1;
+            QuickBind = info[3] == 1;
+            ReserveBind = info[4] == 1;
+            Maggoted = info[5] == 1;
+        }
+    }
+
     /// <summary>
     /// Cached FSM for Hornet's bind ability.
     /// </summary>
-    private static PlayMakerFSM? _bindFsm;
+    protected static PlayMakerFSM? _bindFsm;
 
-    private static GameObject? _localBindEffects;
-    
+    protected static GameObject? _localBindEffects;
+
     public override void Play(GameObject playerObject, CrestType crestType, byte[]? effectInfo) {
-        MonoBehaviourUtil.Instance.StartCoroutine(PlayBindEffect(playerObject, crestType));
+        Flags flags = new Flags(effectInfo);
+        MonoBehaviourUtil.Instance.StartCoroutine(PlayBindEffect(playerObject, crestType, flags));
     }
 
-    private IEnumerator PlayBindEffect(GameObject playerObject, CrestType crestType) {
+    private IEnumerator PlayBindEffect(GameObject playerObject, CrestType crestType, Flags flags) {
         var randomClipAction = GetOrFindBindFsm().GetFirstAction<GetRandomAudioClipFromTable>("Bind Start");
         var playAudioAction = GetOrFindBindFsm().GetFirstAction<PlayAudioEvent>("Bind Start");
-        
+
         AudioUtil.PlayAudioEventWithRandomAudioClipFromTableAtPlayerObject(
             playAudioAction,
             randomClipAction,
@@ -37,70 +61,125 @@ internal class Bind : AnimationEffect {
         var oneShotSingleAction = GetOrFindBindFsm().GetFirstAction<AudioPlayerOneShotSingle>("Check Grounded");
         AudioUtil.PlayAudioOneShotSingleAtPlayerObject(oneShotSingleAction, playerObject);
 
-        _localBindEffects ??= HeroController.instance.gameObject.FindGameObjectInChildren("Bind Effects");
-        if (_localBindEffects == null) {
-            Logger.Warn("Could not find local Bind Effects object in hero object");
+        var created = CreateObjects(playerObject, out var bindEffects);
+        if (!created) {
             yield break;
         }
 
-        var bindEffects = playerObject.FindGameObjectInChildren("Bind Effects");
-        if (bindEffects == null) {
-            Logger.Warn("Player object does not have Bind Effects child, cannot play bind");
-            yield break;
+        // TODO: if Beast is equipped, activate rage burst object (State: Rage Burst?)
+
+        Logger.Info("Determining crest animation...");
+
+        if (crestType == CrestType.Beast) {
+            Logger.Info("Playing Beast Crest Animation");
+            PlayBeastBindStart(bindEffects);
+        } else if (crestType == CrestType.Cursed) {
+            Logger.Info("Playing Cursed Crest Animation");
+        } else if (crestType == CrestType.Witch) {
+            Logger.Info("Playing Witch Crest Animation");
+        } else if (crestType == CrestType.Shaman) {
+            Logger.Info("Playing Shaman Crest Animation");
+        } else {
+            Logger.Info("Playing Default Animation");
+            PlayNormalStart(bindEffects, flags);
         }
 
-        var bindSilkObj = bindEffects.FindGameObjectInChildren("Bind Silk");
-        if (bindSilkObj == null) {
-            var localBindSilkObj = _localBindEffects.FindGameObjectInChildren("Bind Silk");
-            if (localBindSilkObj == null) {
-                Logger.Warn("Could not find local Bind Silk object, cannot play bind");
-                yield break;
+        // If bind bell, do effects in state "Bind Bell?" and "Bind Bell Disappear?"
+        if (flags.BindBell) {
+            StartBindBell(bindEffects);
+        }
+
+        // TODO: If using reserve bind, use reserve bind animation?
+
+        // TODO: Use air animations if applicable
+
+        // TODO: Quick Craft animations
+
+        Logger.Info("Getting clip name for no reason...");
+        var playerAnimator = playerObject.GetComponent<tk2dSpriteAnimator>();
+        var currentClip = playerAnimator?.currentClip;
+        Logger.Info($"Player Animator current clip for Bind: {currentClip?.name}");
+        // TODO: figure out when animation triggers happen
+
+        // TODO: adjust bind time based on crest and tools
+    }
+
+    private void StartBindBell(GameObject bindEffects) {
+        var bindBell = bindEffects.FindGameObjectInChildren(BIND_BELL_NAME);
+        
+        if (bindBell == null) {
+            var allObjects = Resources.FindObjectsOfTypeAll<GameObject>();
+            var localBell = allObjects.FirstOrDefault(o => o.name == "bind_bell_appear");
+            
+            if (localBell == null) {
+                Logger.Warn("Couldn't find bind bell object");
+                return;
             }
 
-            bindSilkObj = Object.Instantiate(localBindSilkObj, bindEffects.transform);
+            bindBell = GameObject.Instantiate(localBell, bindEffects.transform);
+            bindBell.name = BIND_BELL_NAME;
+            
+            var follower = bindBell.GetComponent<FollowTransform>();
+            follower.target = bindEffects.transform;
+            follower.useHero = false;
+
+            var delay = bindBell.AddComponentIfNotPresent<DeactivateAfterDelay>();
+            delay.time = 5f;
+        }
+
+        bindBell.SetActive(false);
+        bindBell.SetActive(true);
+    }
+
+    private void PlayNormalStart(GameObject bindEffects, Flags flags) {
+        var bindSilkObj = CreateEffectIfNotExists(bindEffects, "Bind Silk");
+        if (bindSilkObj == null) {
+            return;
         }
 
         var bindSilkMeshRenderer = bindSilkObj.GetComponent<MeshRenderer>();
         bindSilkMeshRenderer.enabled = true;
 
         var bindSilkAnimator = bindSilkObj.GetComponent<tk2dSpriteAnimator>();
-        bindSilkAnimator.Play(bindSilkAnimator.GetClipByName("Bind Silk"));
-        
-        // TODO: if Beast is equipped, activate rage burst object (State: Rage Burst?)
-        // TODO: if claw mirrors are equipped, do effects in state "Dazzle?"
-        // TODO: if maggoted, do effects in state "Maggoted?"
 
-        var playerAnimator = playerObject.GetComponent<tk2dSpriteAnimator>();
-        var currentClip = playerAnimator.currentClip;
-        Logger.Error($"Player Animator current clip for Bind: {currentClip.name}");
-        // TODO: figure out when animation triggers happen
+        Logger.Info("Playing Bind Silk animation");
 
-        // TODO: adjust bind time based on crest and tools
-        var bindTime = 1.2f;
+        if (flags.QuickBind) bindSilkAnimator.Play(bindSilkAnimator.GetClipByName("Bind Silk Quick"));
+        else bindSilkAnimator.Play(bindSilkAnimator.GetClipByName("Bind Silk"));
+    }
 
-        yield return new WaitForSeconds(bindTime);
+    private void PlayBeastBindStart(GameObject bindEffects) {
+        var beastAntic = CreateEffectIfNotExists(bindEffects, "Warrior_Bind_antic_silk");
+        if (beastAntic == null) {
+            return;
+        }
 
-        bindSilkMeshRenderer.enabled = false;
-        
-        // TODO: activate object in state "Heal", last action
-        
-        
+        beastAntic.SetActive(true);
+
     }
 
     /// <inheritdoc/>
     public override byte[]? GetEffectInfo() {
-        // Warding bell
-        // Claw mirrors
-        return null;
+        byte[] effectInfo = {
+            (byte) (ToolItemManager.IsToolEquipped("Bell Bind") ? 1 : 0),
+            (byte) (ToolItemManager.IsToolEquipped("Dazzle Bind") ? 1 : 0),
+            (byte) (ToolItemManager.IsToolEquipped("Dazzle Bind Upgraded") ? 1 : 0),
+            (byte) (ToolItemManager.IsToolEquipped("Quickbind") ? 1 : 0),
+            (byte) (ToolItemManager.IsToolEquipped("Reserve Bind") ? 1 : 0),
+            (byte) (HeroController.instance.cState.isMaggoted ? 1 : 0),
+            //(byte) (HeroController.instance.onFlatGround ? 0 : 1)
+        };
+
+        return effectInfo;
     }
-    
+
     /// <summary>
     /// Get or find the Bind FSM on the hero object. Will be cached to <see cref="_bindFsm"/>.
     /// </summary>
     /// <returns>The FSM for Bind.</returns>
     /// <exception cref="InvalidOperationException">Thrown if the FSM cannot be found, which shouldn't happen.
     /// </exception>
-    private PlayMakerFSM GetOrFindBindFsm() {
+    protected PlayMakerFSM GetOrFindBindFsm() {
         if (_bindFsm != null) {
             return _bindFsm;
         }
@@ -114,5 +193,38 @@ internal class Bind : AnimationEffect {
         }
 
         throw new InvalidOperationException("Could not find Bind FSM on hero");
+    }
+
+    protected bool CreateObjects(GameObject playerObject, out GameObject bindEffects) {
+        _localBindEffects ??= HeroController.instance.gameObject.FindGameObjectInChildren("Bind Effects");
+        if (_localBindEffects == null) {
+            Logger.Warn("Could not find local Bind Effects object in hero object");
+            bindEffects = null;
+            return false;
+        }
+
+        bindEffects = playerObject.FindGameObjectInChildren("Bind Effects");
+        if (bindEffects == null) {
+            Logger.Warn("Player object does not have Bind Effects child, cannot play bind");
+            return false;
+        }
+
+        return true;
+    }
+
+    protected GameObject? CreateEffectIfNotExists(GameObject bindEffects, string objectName) {
+        var obj = bindEffects.FindGameObjectInChildren(objectName);
+        if (obj == null) {
+            var localObj = _localBindEffects.FindGameObjectInChildren(objectName);
+            if (localObj == null) {
+                Logger.Warn($"Could not find local {objectName} object, cannot play bind");
+                return null;
+            }
+
+            obj = Object.Instantiate(localObj, bindEffects.transform);
+            obj.name = objectName;
+        }
+
+        return obj;
     }
 }
---
+++ SSMP/Animation/Effects/BindBurst.cs
@@ -0,0 +1,158 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using HutongGames.PlayMaker.Actions;
+using SSMP.Internals;
+using SSMP.Util;
+using UnityEngine;
+using Logger = SSMP.Logging.Logger;
+using Object = UnityEngine.Object;
+
+namespace SSMP.Animation.Effects;
+
+internal class BindBurst : Bind {
+    public override void Play(GameObject playerObject, CrestType crestType, byte[]? effectInfo) {
+        Flags flags = new Flags(effectInfo);
+        MonoBehaviourUtil.Instance.StartCoroutine(PlayBindBurstEffect(playerObject, crestType, flags));
+    }
+
+    private IEnumerator PlayBindBurstEffect(GameObject playerObject, CrestType crestType, Flags flags) {
+        if (!CreateObjects(playerObject, out var bindEffects)) {
+            yield break;
+        }
+
+        if (flags.BaseMirror || flags.UpgradedMirror) PlayMirror(bindEffects, flags.UpgradedMirror);
+        if (flags.BindBell) StopBindBell(bindEffects);
+        if (flags.Maggoted) PlayMaggotCleanse(bindEffects, playerObject);
+
+        switch (crestType) {
+            case CrestType.Beast:
+                PlayBeastRage(bindEffects);
+                break;
+                //yield break;
+            default:
+                break;
+        }
+
+        PlayNormalEnd(bindEffects);
+    }
+
+    private GameObject? PrepareMirror(GameObject bindEffects, SetGameObject mirrorSource, string name) {
+        var mirror = bindEffects.FindGameObjectInChildren(name);
+        if (mirror != null) {
+            mirror.SetActive(false);
+            return mirror;
+        }
+
+        if (mirrorSource == null) {
+            Logger.Warn("Unable to find mirror source");
+            return null;
+        }
+
+        mirror = mirrorSource.gameObject.Value.Spawn(bindEffects.transform, Vector3.zero);
+        if (mirror == null) {
+            Logger.Warn("Unable to spawn mirror");
+            return null;
+        }
+        mirror.name = name;
+
+        var shaker = mirror.GetComponentInChildren<CameraControlAnimationEvents>();
+        if (shaker != null) {
+            Component.DestroyImmediate(shaker);
+        }
+        var delay = mirror.AddComponentIfNotPresent<DeactivateAfterDelay>();
+        delay.time = 2f;
+        EffectUtils.SafelyRemoveAutoRecycle(mirror);
+        var haze = mirror.FindGameObjectInChildren("haze2");
+        if (haze != null) {
+            GameObject.Destroy(haze);
+        }
+
+        mirror.SetActive(false);
+        return mirror;
+    }
+
+    private void PlayMirror(GameObject bindEffects, bool upgraded) {
+        Logger.Info("Playing Claw Mirror Animation");
+        var regularClaw = GetOrFindBindFsm().GetAction<SetGameObject>("Dazzle?", 3)!;
+        var upgradedClaw = GetOrFindBindFsm().GetAction<SetGameObject>("Dazzle?", 4)!;
+
+        GameObject? claw;
+        if (upgraded) claw = PrepareMirror(bindEffects, upgradedClaw, "dazzle_upgraded");
+        else claw = PrepareMirror(bindEffects, regularClaw, "dazzle_regular");
+
+        if (claw == null) {
+            Logger.Warn("Unable to create claw mirror object.");
+            return;
+        }
+
+        claw.SetActive(true);
+        if (ServerSettings.IsPvpEnabled && ShouldDoDamage) {
+            var damagerParent = claw.FindGameObjectInChildren("Trobbio_dazzle_flash");
+            var damager = damagerParent?.FindGameObjectInChildren("hero_dazzle_flash_damager");
+            if (damager != null) {
+                damager.layer = (int) GlobalEnums.PhysLayers.HERO_ATTACK;
+                AddDamageHeroComponent(damager);
+            } else {
+                Logger.Warn("Couldn't find claw mirror damager");
+            }
+        } else {
+            var damager = claw.GetComponentInChildren<DamageHero>(true);
+            if (damager != null) {
+                Component.Destroy(damager);
+            }
+        }
+    }
+
+    private void StopBindBell(GameObject bindEffects) {
+        var bindBell = bindEffects.FindGameObjectInChildren(BIND_BELL_NAME);
+        if (bindBell != null) {
+            bindBell.SetActive(false);
+        }
+    }
+
+    private void PlayMaggotCleanse(GameObject bindEffects, GameObject playerObject) {
+        Logger.Info("Playing Maggot Animation");
+        var maggotBurst = GetOrFindBindFsm().GetAction<SpawnObjectFromGlobalPool>("Maggoted?", 2);
+        var maggotFlash = GetOrFindBindFsm().GetAction<SpawnObjectFromGlobalPool>("Maggoted?", 4);
+        var maggotAudio = GetOrFindBindFsm().GetFirstAction<PlayAudioEvent>("Maggoted?");
+
+        if (maggotBurst != null) {
+            maggotBurst.gameObject.Value.Spawn(bindEffects.transform, Vector3.zero);
+        }
+        if (maggotFlash != null) {
+            maggotFlash.gameObject.Value.Spawn(bindEffects.transform, Vector3.zero);
+        }
+        if (maggotAudio != null) {
+            AudioUtil.PlayAudioEventAtPlayerObject(
+                maggotAudio,
+                playerObject
+            );
+        }
+    }
+
+    private void PlayBeastRage(GameObject bindEffects) {
+        var beastRage = CreateEffectIfNotExists(bindEffects, "crest rage_burst_effect(Clone)");
+        beastRage?.SetActive(true);
+    }
+
+    private void PlayNormalEnd(GameObject bindEffects) {
+        Logger.Info("Playing Heal Particles and Anim");
+        var healParticle = CreateEffectIfNotExists(bindEffects, "Pt Heal");
+        if (healParticle != null) {
+            healParticle.GetComponent<ParticleSystem>().Play();
+        }
+        
+        var healAnim = CreateEffectIfNotExists(bindEffects, "Heal Anim");
+        if (healAnim != null) {
+            healAnim.SetActive(true);
+        }
+
+        var bindSilkObj = bindEffects.FindGameObjectInChildren("Bind Silk");
+        if (bindSilkObj != null) {
+            var bindSilkMeshRenderer = bindSilkObj.GetComponent<MeshRenderer>();
+            bindSilkMeshRenderer.enabled = false;
+        }
+    }
+}
